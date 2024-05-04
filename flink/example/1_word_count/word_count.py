5c6b96c2838a8f4f06ca8af8d652ee7e108cb74c
hunrizideABC <hunrizideabc@gmail.com>
2024-05-03 21:54:28 -0700

    test

--- flink/example/1_word_count/word_count.py
+++ flink/example/1_word_count/word_count.py
@@ -1,48 +1,38 @@
-import os
-import shutil
-from pyflink.table import BatchTableEnvironment, EnvironmentSettings
-from pyflink.table import DataTypes
-from pyflink.table.descriptors import Schema, OldCsv, FileSystem
+from pyflink.table import EnvironmentSettings, TableEnvironment
 
-env_settings = EnvironmentSettings.new_instance().in_batch_mode().use_blink_planner().build()
-t_env = BatchTableEnvironment.create(environment_settings=env_settings)
-dir_word = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'input_file')
-dir_result = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'result_file')
-# 如果文件/文件夹存在，则删除
-if os.path.exists(dir_result):
-    if os.path.isfile(dir_result):
-        os.remove(dir_result)
-    else:
-        shutil.rmtree(dir_result, True)
+environment_settings = EnvironmentSettings.new_instance().use_blink_planner().in_batch_mode().build()
+t_env = TableEnvironment.create(environment_settings=environment_settings)
+t_env.get_config().get_configuration().set_string('parallelism.default', '1')
 
-t_env.execute_sql(f"""
-    CREATE TABLE source (
-        id BIGINT,     -- ID
-        word STRING    -- 单词
-    ) WITH (
-        'connector' = 'filesystem',
-        'path' = 'file://{dir_word}',
-        'format' = 'csv'
-    )
-""")
+t_env.execute_sql("""
+         CREATE TABLE testSource (
+           word STRING
+         ) WITH (
+           'connector' = 'filesystem',
+           'format' = 'csv',
+           'path' = '/opt/flink/recommendation-flink/flink/example/1_word_count/input_file'
+         )
+     """)
 
-t_env.execute_sql(f"""
-    CREATE TABLE sink (
-        word STRING,   -- 单词
-        cnt BIGINT     -- 出现次数
-    ) WITH (
-        'connector' = 'filesystem',
-        'path' = 'file://{dir_result}',
-        'format' = 'csv'
-    )
-""")
+t_env.execute_sql("""
+         CREATE TABLE testSink (
+           word STRING,
+           `count` BIGINT
+         ) WITH (
+           'connector' = 'filesystem',
+           'format' = 'csv',
+           'path' = '/opt/flink/recommendation-flink/flink/example/1_word_count/output_file'
+         )
+     """)
 
-t_env.sql_query("""
-    SELECT word
-           , count(1) AS cnt
-    FROM source
-    GROUP BY word
-""").insert_into('sink')
-t_env.execute('t')
+t_env.from_path('testSource') \
+    .group_by('word') \
+    .select('word, count(1)') \
+    .insert_into('testSink')
+
+# Execute
+t_env.execute("1-word_count")
+
+
 
 
